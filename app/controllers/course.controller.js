7ce7579e00c82c24241af527846bd259261bb360
40231676 <jgibson49@qub.ac.uk>
2022-01-27 13:57:35 +0000

    created controller for getting course ratings

---
+++ app/controllers/course.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models"); 
+const Courses = db.course;
+const Course_Ratings = db.course_rating;
+
+exports.getCourseRating = async (req, res) => { 
+    let courseId = req.params.courseId; 
+
+    const totalRatings = await Course_Ratings.count({ where: { courseId: courseId} }).then(count => count);
+    const ratingTotal = await Course_Ratings.sum('rating', { where: { courseId: courseId} }).then(sum => sum);
+    const roundedAverage = Math.round((ratingTotal / totalRatings) * 2) / 2;
+    return res.json({ rating: roundedAverage });
+};
+
+exports.getAllCourseRatings = async (req, res) => { 
+    let ratings = [];
+
+    await Courses.findAll().then(async (courses) => { 
+        for (const course of courses) {
+            const courseId = course.dataValues.id;
+            const totalRatings = await Course_Ratings.count({ where: { courseId: courseId} }).then(count => count);
+            const ratingTotal = await Course_Ratings.sum('rating', { where: { courseId: courseId} }).then(sum => sum);
+
+            if (totalRatings == 0) { 
+                ratings.push({ courseId: courseId, rating: 0});
+                continue;
+            };
+            
+            const roundedAverage = Math.round((ratingTotal / totalRatings) * 2) / 2;
+            ratings.push({ courseId: courseId, rating: roundedAverage});
+        };
+        return res.json(ratings);
+    }).catch((err) => { 
+        return res.status(500).send({ message: err.message });
+    });
+};
---
+++ app/routes/course.routes.js
@@ -0,0 +1,24 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/course.controller"); 
+
+module.exports = function(app) { 
+    app.use(function(req, res, next) { 
+        res.header( 
+            "Access-Control-Allow-Headers", 
+            "x-access-token, Origin, Content-Type, Accept"
+        ); 
+        next();
+    }); 
+
+    app.get(
+        "/api/rating/:courseId", 
+        [authJwt.verifyToken], 
+        controller.getCourseRating
+    );
+
+    app.get( 
+        "/api/ratings", 
+        [authJwt.verifyToken], 
+        controller.getAllCourseRatings
+    )
+};
