405b5cbdf84a6d1d39a9978b8571fb415b6689ab
40231676 <jgibson49@qub.ac.uk>
2022-02-04 14:58:31 +0000

    quiz model and junction table defined - now to create controller

--- app/models/userQuizScores.model.js
+++ app/models/userQuizScores.model.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, Sequelize) => { 
     const UserQuizScores = sequelize.define("user_quiz_score", { 
-        bestScore: Sequelize.DataTypes.INTEGER
+        bestScore: { 
+            type: Sequelize.DataTypes.INTEGER, 
+            allowNull: false, 
+        }
     }, { timestamps: false });
     
     return UserQuizScores;
