0c2ea5e2aa878ccec03450145fadf97fcd0b8893
40231676 <jgibson49@qub.ac.uk>
2022-02-16 23:25:36 +0000

    admin get all users now contains credit field

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -15,6 +15,7 @@ exports.getAllUsers = async (req, res) => {
             'username', 
             'email', 
             'createdAt', 
+            [sequelize.literal('(SELECT credits FROM credits WHERE users.id = credits.userId)'), 'credits'], 
             [sequelize.literal('(SELECT COUNT(DISTINCT(courseId)) FROM user_completed_courses WHERE users.id = user_completed_courses.userId)'), 'coursesComplete'], 
             [sequelize.literal('(SELECT COUNT(*) FROM user_quiz_scores WHERE users.id = user_quiz_scores.userId AND user_quiz_scores.bestScore > -1)'), 'quizzesComplete']
         ] 
