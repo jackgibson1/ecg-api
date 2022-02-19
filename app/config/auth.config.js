12b5af2815fa368437ac437895ef3c1a26943664
40231676 <jgibson49@qub.ac.uk>
2022-02-19 15:52:20 +0000

    updating jwt expiration times

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,10 +1,8 @@
 // exports JWT secret key from environment variable
 module.exports = { 
     secret: process.env.JWT_SECRET,
-    // jwtExpiration: 3600, // 1 hour 
-    // jwtRefreshExpiration: 86400, // 24 hours 
+    jwtExpiration: 7200, // 2 hours
+    jwtRefreshExpiration: 86400, // 24 hours 
 
-    jwtExpiration: 30, // 1 minute
-    jwtRefreshExpiration: 60, // 2 minutes
     
 } 
