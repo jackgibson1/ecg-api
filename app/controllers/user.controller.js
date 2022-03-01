70f94950f17197e7c812481c2283a0db75f86ff8
40231676 <jgibson49@qub.ac.uk>
2022-03-01 20:43:44 +0000

    linting user and updating CI/CD pipeline to include linting stage

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,50 +1,47 @@
-/* 
- * Controller for all user related logic 
+/*
+ * Controller for all user related logic
  * Date user created account & users total credits
  * Note: Logic for incrementation of user credits is done within course/quiz controllers
 */
 
-const db = require("../models");
+const db = require('../models');
+
 const Users = db.user;
 const Credit = db.credit;
 
 // Get date that user created account
-exports.getDateCreated = (req, res) => { 
-    let userId = req.headers["user-id"]; 
-    
-    if(!userId) { 
-        return res.status(403).send({ 
-            message: "No userId provided!"
-        });
-    }
+exports.getDateCreated = (req, res) => {
+  const userId = req.headers['user-id'];
 
-    Users.findOne({ where: { id: userId }})
-    .then((result) => { 
-        let dateObj = new Date(result.dataValues.createdAt);
-        res.json({ created: dateObj.toLocaleDateString('en-UK') });
+  if (!userId) {
+    return res.status(403).send({
+      message: 'No userId provided!'
+    });
+  }
+
+  Users.findOne({ where: { id: userId } })
+    .then((result) => {
+      const dateObj = new Date(result.dataValues.createdAt);
+      res.json({ created: dateObj.toLocaleDateString('en-UK') });
     })
-    .catch((err) => { 
-        res.status(500).send({ message: err.message });
-    })   
-}
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
 
 // Get total credits that user has earned
-exports.getCredits = (req, res) => { 
-    let userId = req.headers["user-id"]; 
-    
-    if(!userId) { 
-        return res.status(403).send({ 
-            message: "userId not present!"
-        });
-    }
+exports.getCredits = (req, res) => {
+  const userId = req.headers['user-id'];
 
-    Credit.findAll({
-        where: { 
-            userId: userId, 
-        }
-    }).then(result => { 
-        return res.json({ credits: result[0].dataValues.credits });
-    }).catch((err) => { 
-        return res.status(500).send({ message: err.message });
+  if (!userId) {
+    return res.status(403).send({
+      message: 'userId not present!'
     });
+  }
+
+  Credit.findAll({
+    where: {
+      userId
+    }
+  }).then((result) => res.json({ credits: result[0].dataValues.credits })).catch((err) => res.status(500).send({ message: err.message }));
 };
